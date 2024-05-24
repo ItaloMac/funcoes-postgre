40ee5c905c2dc3405ef3845444b64338928a66a2
italo macedo <macedoitalo21@gmail.com>
2024-05-23 22:18:41 -0300

    Resolução Query 6

--- query.sql
+++ query.sql
@@ -3,3 +3,4 @@ SELECT MIN(idade) from usuarios,
 SELECT MAX(idade) from usuarios,
 SELECT AVG(idade) AS media_idade from usuarios WHERE idade >= 18,
 SELECT SUM(estoque) from farmacia WHERE categoria IN('blue','black'),
+SELECT categoria, SUM(estoque) AS total_estoque FROM farmacia WHERE categoria IS NOT NULL GROUP BY categoria;
